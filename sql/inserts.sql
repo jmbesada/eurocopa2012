17815a28409cd59b2e4c21e4fa592fa5839e4e29
jmbesada <jmbesada@dev.alphatool.es>
2012-05-28 18:38:58 +0200

    sdfssdsd

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -20,4 +20,20 @@ insert into t_countries (ID, VERSION, NAME, GROUP_ID, ICON_PATH) values (14, 1, 
 insert into t_countries (ID, VERSION, NAME, GROUP_ID, ICON_PATH) values (15, 1, 'Suecia', 4, 'Sweden.png');
 insert into t_countries (ID, VERSION, NAME, GROUP_ID, ICON_PATH) values (16, 1, 'Ucrania', 4, 'Ukraine.png');
 
+
 insert into t_users (ID, VERSION, EMAIL, password) values (1, 5, 'jmbesada@qwi-ti.com', '525700');
+insert into t_users (ID, VERSION, EMAIL, password) values (2, 5, 'portiz@gmail.com', 'portiz@gmail.com');
+insert into t_users (ID, VERSION, EMAIL, password) values (3, 6, 'antonio.urbano@qwi-ti.com', 'comidagratis');
+insert into t_users (ID, VERSION, EMAIL, password) values (4, 5, 'demo', 'demo');
+insert into t_users (ID, VERSION, EMAIL, password) values (5, 5, 'daniel.gomez@qwi-ti.com', 'daniel.gomez@qwi-ti.com');
+insert into t_users (ID, VERSION, EMAIL, password) values (6, 5, 'gaspar.hernandez@qwi-ti.com', 'gaspar.hernandez@qwi-ti.com');
+insert into t_users (ID, VERSION, EMAIL, password) values (7, 6, 'jlopez@qwi-ti.com', 'jl0p3z');
+insert into t_users (ID, VERSION, EMAIL, password) values (8, 6, 'mgarcia@qwi-ti.com', 'mg4rc14');
+insert into t_users (ID, VERSION, EMAIL, password) values (9, 5, 'mmarugan@qwi-ti.com', 'mmarugan@qwi-ti.com');
+insert into t_users (ID, VERSION, EMAIL, password) values (10, 5, 'raquel.plaza@qwi-ti.com', 'raquel.plaza@qwi-ti.com');
+insert into t_users (ID, VERSION, EMAIL, password) values (11, 5, 'smontoiro@qwi-ti.com', 'smontoiro@qwi-ti.com');
+insert into t_users (ID, VERSION, EMAIL, password) values (12, 5, 'susana.montero@qwi-ti.com', 'susana.montero@qwi-ti.com');
+insert into t_users (ID, VERSION, EMAIL, password) values (13, 5, 'jorge.montero@qwi-ti.com', 'jorge.montero@qwi-ti.com');
+insert into t_users (ID, VERSION, EMAIL, password) values (14, 5, 'jbesada@qwi-ti.com', 'jbesada@qwi-ti.com');
+insert into t_users (ID, VERSION, EMAIL, password) values (15, 5, 'gplaza@qwi-ti.com', 'gplaza@qwi-ti.com');
+
